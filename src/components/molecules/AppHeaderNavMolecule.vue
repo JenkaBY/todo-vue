ddcc940ef975d2342170f1d4d33f29a0086371f6
y.kuzmich <evgen.kuzmich@gmail.com>
2024-08-27 19:42:01 +0300

    feat: add basic project elements according to atomic design

---
+++ src/components/molecules/AppHeaderNavMolecule.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppTitleAtom from '@/components/atoms/AppTitleAtom.vue'
+
+defineProps<{
+  applicationTitle: string
+}>()
+const moonUnicode = '&#9790;'
+</script>
+
+<template>
+  <div class="flex flex-row justify-between">
+    <AppTitleAtom :application-title="applicationTitle" />
+    <span v-html="moonUnicode" class="text-3xl items-center"></span>
+  </div>
+</template>
+
+<style scoped></style>
