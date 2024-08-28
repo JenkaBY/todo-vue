0136bc04fd0ec60f1817811bc3c25eadd9b98b94
y.kuzmich <evgen.kuzmich@gmail.com>
2024-08-28 20:10:50 +0300

    feat: create base todo task list component

---
+++ src/components/molecules/TodoTasksList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const items = reactive<number[]>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+</script>
+
+<template>
+  <ul >
+    <li v-for="item in items" :key="item">Todo {{ item }}</li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
