e55d1587ced6ba96be09251fdff4906ec20b4381
y.kuzmich <evgen.kuzmich@gmail.com>
2024-08-28 20:10:16 +0300

    feat: add new task input

---
+++ src/components/atoms/TodoInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface TodoInputProps {
+  onEnterPress?: Function,
+  onKeyPress?: Function,
+}
+
+const props:TodoInputProps = withDefaults(defineProps<TodoInputProps>(), {
+  onEnterPress: (event) => console.log('Default onEnterPress event:', event.target.value),
+  onKeyPress: (event) => console.log('Default onKeyPress event:', event.target.value),
+  currentTask: ''
+}) as TodoInputProps
+
+</script>
+
+<template>
+  <input type="text" placeholder="Enter a new task..."
+         class="focus:outline-none"
+         @keyup="props.onKeyPress"
+         @keyup.enter="props.onEnterPress">
+</template>
+
+<style scoped></style>
---
+++ src/components/molecules/TodoInputTask.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import TodoInput from '@/components/atoms/TodoInput.vue'
+import TaskStatusIcon from '@/components/molecules/TaskStatusIcon.vue'
+import { ref } from 'vue'
+
+interface TodoInputTaskProps {
+  isTaskCompleted: boolean,
+}
+
+withDefaults(defineProps<TodoInputTaskProps>(), {
+  isTaskCompleted: false
+})
+
+const isValidTask = /^(?!\s*$).+/
+const isInvalidTask = ref<boolean>(false)
+
+const addNewTask = (event) => {
+  console.log('New task added:', event.target.value)
+  if (!isValidTask.test(event.target.value)) {
+    console.log('Invalid task added')
+    isInvalidTask.value = true
+    return
+  }
+  event.target.value = ''
+  isInvalidTask.value = false
+}
+
+const onKeyPress = (event) => {
+  console.log('on key Pressed:', event.target.value)
+  console.log('on key Pressed length:', event.target.value.length )
+  console.log('on key Pressed length <=:', event.target.value.length <= 150)
+  const validatedValue = event.target.value
+  if (isInvalidTask.value && isValidTask.test(validatedValue) && validatedValue.length <= 150) {
+    isInvalidTask.value = false
+    return
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-row justify-between p-2 bg-white rounded-lg"
+       :class="{ 'border border-pink-500': isInvalidTask }">
+    <TaskStatusIcon :completed="false" :inactive="true" class="m-1.5" />
+    <TodoInput class="w-full pl-2 pr-2" :onEnterPress="addNewTask" :onKeyPress="onKeyPress"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
