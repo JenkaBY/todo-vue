5c0b83d0fb3433428544503e47864cf73d01277d
y.kuzmich <evgen.kuzmich@gmail.com>
2024-08-29 18:22:55 +0300

    feat: create todoTask store

---
+++ src/stores/todaTasks.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+
+export const useTodoTasksStore = defineStore('todoTasks', {
+  state: () => {
+    return {
+      tasks: [] as TodoTask[],
+      filter: 'ALL' as 'ALL' | 'COMPLETED' | 'READY_TO_START',
+      nextId: 0
+    }
+  },
+  getters: {
+    filterTodoTasks(state) {
+      if (state.filter === 'ALL') {
+        return state.tasks;
+      }
+      return state.tasks.filter(task => task.status.toString() === state.filter)
+    }
+
+  },
+  actions: {
+    addTask(description: string) {
+      this.tasks.push({ description, id: this.nextId++, status: TodoTaskStatus.READY_TO_START })
+    },
+    removeTask(task: TodoTask) {
+      const index = this.tasks.indexOf(task);
+      this.tasks.splice(index, 1)
+    }
+  },
+})
+
+export interface TodoTask {
+  id: number;
+  description: string;
+  status: TodoTaskStatus;
+}
+
+export enum TodoTaskStatus {
+  COMPLETED,
+  READY_TO_START,
+}
