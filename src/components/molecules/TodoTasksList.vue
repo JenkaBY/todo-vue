5e442a0bd65c46c0087a51862bd2f7fdcf53d4cd
y.kuzmich <evgen.kuzmich@gmail.com>
2024-08-31 15:41:15 +0300

    feat: implement draggable list

--- src/components/molecules/TodoTasksList.vue
+++ src/components/molecules/TodoTasksList.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
 import { type TodoTask, useTodoTasksStore } from '@/stores/todoTasksStore'
 import TodoItem from '@/components/molecules/TodoItem.vue'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import draggable from 'vuedraggable'
 
+const dragging = ref<boolean>(false)
 const todoTasksStore = useTodoTasksStore()
 const tasks = reactive<TodoTask[]>(todoTasksStore.filterTodoTasks)
 </script>
 
 <template>
-  <ul
+  <draggable
+    tag="ul"
+    :list="tasks"
+    group="tasks"
+    @start="dragging = true"
+    @end="dragging = false"
+    item-key="element.id"
     class="bg-white rounded-t-lg rounded-b-lg shadow-xl shadow-slate-200 divide-y divide-slate-200"
   >
-    <li v-for="task in tasks" :key="task.id" class="first:rounded-t-lg last:rounded-b-lg bg-white">
-      <TodoItem v-bind="task" class="task-item" />
-    </li>
-  </ul>
+    <template #item="{ element }">
+      <TodoItem
+        v-bind="element"
+        class="task-item first:rounded-t-lg last:rounded-b-lg bg-white cursor-move"
+      />
+    </template>
+  </draggable>
 </template>
 
 <style scoped></style>
