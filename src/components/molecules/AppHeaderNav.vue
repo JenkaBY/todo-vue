4242c9b42747fad7d3e4be309cf1e51056ee0642
y.kuzmich <evgen.kuzmich@gmail.com>
2024-09-04 16:02:41 +0300

    refactor: address comments

--- src/components/molecules/AppHeaderNav.vue
+++ src/components/molecules/AppHeaderNav.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import { inject } from 'vue'
-
-import { providerFunctionName } from '@/constants/constants'
-
+import { ref } from 'vue'
+import { app } from '@/constants/constants'
 import AppTitle from '@/components/atoms/AppTitle.vue'
 
-const applicationTitle = inject<string>(providerFunctionName.applicationTitle, 'appName')
-const moonUnicode = '&#9790;'
+const applicationTitle = ref<string>(app.title)
 </script>
 
 <template>
   <div class="flex flex-row justify-between">
     <AppTitle :application-title="applicationTitle" />
-    <span v-html="moonUnicode" class="text-3xl items-center"></span>
+    <span class="text-3xl items-center">☾</span>
   </div>
 </template>
 
--- src/components/molecules/TodoInputTask.vue
+++ src/components/molecules/TodoInputTask.vue
@@ -26,19 +26,14 @@ const rules = computed(() => ({
 }))
 const $v = useVuelidate(rules, { inputTodoTaskValue })
 
-const addNewTask = () => {
-  const result = $v.value.$validate()
-  result
-    .then((res) => {
-      if (res) {
+const addNewTask = async () => {
+  const result = await $v.value.$validate()
+
+      if (result) {
         todoTasksStore.addTask(inputTodoTaskValue.value)
         inputTodoTaskValue.value = ''
         $v.value.$reset()
       }
-    })
-    .catch((err) => {
-      console.log('An error occurred', err)
-    })
 }
 </script>
 
